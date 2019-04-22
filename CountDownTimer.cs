54495ed567a5f02d79ea548472436cbeb53a03cb
Nikhilesh <reachnikhilesh@gmail.com>
2019-04-22 12:47:52 +0530

    Create CountDownTimer.cs
    Creating file

---
+++ CountDownTimer.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testConsoleApp
+{
+    public class CountDownTimer
+    {
+        TimeSpan duration;
+        bool started;
+        DateTime startTime;
+        DateTime endTime;
+        /// <summary>
+        /// Constructor sets up a countdown timer.
+        /// </summary>
+        /// <param name="set">TimeSpan for the countdown</param>
+        public CountDownTimer(TimeSpan set) 
+        {
+            this.duration = set;
+        }
+        /// <summary>
+        /// Starts timer.
+        /// </summary>
+        public void Start()
+        {
+            this.started = true;
+            this.startTime = DateTime.Now;
+            this.endTime = this.startTime.Add(this.duration);
+        }
+
+        /// <summary>
+        /// Retuns the time left in count down.
+        /// 
+        /// Exception: Throws an expecption when the timer has not started.
+        /// </summary>
+        /// <returns></returns>
+        public TimeSpan Remaining()
+        {
+            if (this.started)
+            {
+                return this.endTime - DateTime.Now;
+            }
+            else
+            {
+                throw new Exception("Timer is yet to start");
+            }
+        }
+        
+    }
+}
